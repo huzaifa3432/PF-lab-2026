3146551fb3abed3255b4f91094eb60c1629e9911
huzaifa3432 <huzaifatahir3432@gmail.com>
2026-02-15 20:03:27 +0500

    Create task1.c

---
+++ lab 04/task1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float att;
+	printf("Enter attendnce in %: ");
+	scanf("%f", &att)
+	if(att>=75){
+		printf("Selected for Tournament");
+	}
+	else
+	printf("Not selected");
+	return 0;
+}
